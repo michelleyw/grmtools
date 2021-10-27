7c451f328520d32369593e6e5972c8eb6393d5b8
Laurence Tratt <laurie@tratt.net>
2021-10-27 19:12:44 +0100

    Mea culpa

--- .buildbot.sh
+++ .buildbot.sh
@@ -8,6 +8,8 @@ export RUSTUP_HOME="`pwd`/.rustup"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > rustup.sh
 sh rustup.sh --default-host x86_64-unknown-linux-gnu --default-toolchain stable -y --no-modify-path
 
+export PATH=`pwd`/.cargo/bin/:$PATH
+
 cargo fmt --all -- --check
 
 rustup toolchain install stable
